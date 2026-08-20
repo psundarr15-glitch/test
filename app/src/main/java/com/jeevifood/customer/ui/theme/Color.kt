3e8b02af41697600c3bbfc784f180c060c240267
psundarr15-glitch <psundarr15@gmail.com>
2026-08-20 11:01:03 +0530

    Create Color.kt

---
+++ app/src/main/java/com/jeevifood/customer/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.jeevifood.customer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val JeeviRed = Color(0xFFE63946)
+val JeeviRedDark = Color(0xFFB4232E)
+val JeeviCream = Color(0xFFFFF8F0)
+val JeeviGreen = Color(0xFF2A9D8F)
+val JeeviGrey = Color(0xFF6B6B6B)
+val JeeviSurface = Color(0xFFFFFFFF)
